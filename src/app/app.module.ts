22552a7d8221335299be1003a7af7cc238852c58
Ixabel Justo <miju355@vidalibarraquer.net>
2022-06-09 17:34:04 +0200

    JSON

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    PersonajesComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/personajes/personajes.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css'],
+})
+export class PersonajesComponent implements OnInit {
+  pjs: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  devolver() {
+    return [
+      {
+        id: 1,
+        name: 'Rick Sanchez',
+        status: 'Alive',
+        species: 'Human',
+        gender: 'Male',
+        origin: 'Earth',
+        location: 'Earth',
+        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      },
+      {
+        id: 2,
+        name: 'Morty Smith',
+        status: 'Alive',
+        species: 'Human',
+        gender: 'Male',
+        origin: 'Citadel of Ricks',
+        location: 'Earth',
+        image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+      },
+      {
+        id: 3,
+        name: 'Summer Smith',
+        status: 'Alive',
+        species: 'Human',
+        gender: 'Female',
+        origin: 'Earth (Replacement Dimension)',
+        image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+      },
+      {
+        id: 4,
+        name: 'Beth Smith',
+        status: 'Dead',
+        species: 'Human',
+        gender: 'Female',
+        origin: 'Earth (Replacement Dimension)',
+        location: 'Earth',
+        image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+      },
+      {
+        id: 5,
+        name: 'Jerry Smith',
+        status: 'Dead',
+        species: 'Human',
+        gender: 'Male',
+        origin: 'Earth (Replacement Dimension)',
+        location: 'Earth',
+        image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+      },
+    ];
+  }
+
+  ngOnInit() {
+    this.pjs = this.pjs.devolver();
+
+  }
+}
